05e4281ad55c640a13f9455fe9f7a0657e2cc68f
Stephen Buchanan <stephen@mannmademedia.com>
2015-01-29 16:20:06 +0200

    Takes dpi and destination path as paramters

--- svgtopng.sh
+++ svgtopng.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 COUNT=0
-TOTAL="$#"
 
-for var in "$@"
+echo Looking in path $1
+echo Converting to dpi $2
+echo Sending to path $3
+
+DPI=$2
+DESTPATH=$3
+FILEARRAY=( $(find $1/* ) )
+TOTAL="${#FILEARRAY[@]}"
+
+mkdir $3
+
+# echo ${FILEARRAY[@]}
+
+for var in ${FILEARRAY[@]}
 do
 	s=${var##*/}
 	base=${s%.svg}
-	echo 'converting' $var ' to ' $base.png
-	# inkscape -f $var -d=180 -e $base.png
+	echo 'converting' $var ' to ' $DESTPATH/$base.png
+	inkscape -f $var -d=$DPI -e $DESTPATH/$base.png
 	let COUNT=COUNT+1
+	# PERCENTAGE="$[COUNT/TOTAL*100]" | bc
 	echo completed $COUNT of $TOTAL
 	echo '*****'
 done
+
+echo "All done! Output sent to $2"
