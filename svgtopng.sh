214abd471f1b27b42aaf0b17e86db7d5967573c2
Stephen Buchanan <stephen@mannmademedia.com>
2015-01-29 15:08:48 +0200

    counts completed items

--- svgtopng.sh
+++ svgtopng.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+COUNT=0
+
 for var in "$@"
 do
 	s=${var##*/}
 	base=${s%.svg}
-	echo 'converting' $var
-	inkscape -f $var -d=180 -e $base.png
-	echo 'done!'
+	echo 'converting' $var ' to ' $base.png
+	# inkscape -f $var -d=180 -e $base.png
+	let COUNT=COUNT+1
+	echo 'completed ' $COUNT
 	echo '*****'
 done
